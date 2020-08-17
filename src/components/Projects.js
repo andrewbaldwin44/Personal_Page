d1ab2f09d060d95259ebdf989b46bd99ebc0e498
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-16 20:45:58 -0400

    Add carousel to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    }
+  ]
+};
+
 function Projects({ projects }) {
   return (
     <ProjectsSection id="projects-section">
       <h2>Here are Some of my Projects!</h2>
-      <ProjectsContainer>
+      <StyledSlider {...settings}>
         {projects.map(project => {
           const normalizedProjectTitle = project.title.toLowerCase().replace(' ', '');
 
@@ -25,15 +66,17 @@ function Projects({ projects }) {
             </TileItem>
           )
         })}
-      </ProjectsContainer>
+      </StyledSlider>
     </ProjectsSection>
   )
 }
 
 const ProjectsSection = styled.section`
-  left: 0;
+  position: relative;
   width: 100vw;
+  height: calc(100vh - var(--navbar-height));
   background-color: var(--orangeBG);
+  padding: 0 20px;
 
   h2 {
     text-decoration: underline;
@@ -45,9 +88,15 @@ const ProjectsSection = styled.section`
   }
 `;
 
-const ProjectsContainer = styled.div`
+const StyledSlider = styled(Slider)`
+  position: absolute;
   display: flex;
-  flex-wrap: wrap;
+  align-items: center;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 90vw;
+  height: 60%;
+  bottom: 10%;
 `;
 
 const TileItem = styled.div`
@@ -55,26 +104,20 @@ const TileItem = styled.div`
   flex-direction: column;
   box-sizing: border-box;
   background-color: white;
-  text-align: center;
   border-radius: 10px;
-  width: calc(100% / 3 - 4vw);
-  height: auto;
+  height: 100%;
+  max-width: 90%;
   margin-bottom: 100px;
   margin-right: 2vw;
   margin-left: 2vw;
   cursor: pointer;
-  transition: all 0.8s ease-in-out;
-
-  &:hover {
-    transform: scale(1.01);
-    box-shadow: 0 0 8px 8px var(--shadow);
-  }
 `;
 
 const TileImage = styled.img`
   width: 80%;
-  height: auto;
+  max-height: 400px;
   padding-top: 10px;
+  margin: 0 auto;
 `;
 
 const TitleBox = styled.div`
