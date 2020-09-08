939bc00ffef41e9cf45169d0f6ab7ec1f36dcfca
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-08 17:56:21 -0400

    Add navlink animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,10 +16,22 @@ function NavBar() {
       <div className='nav-menu'>
         <GiHamburgerMenu />
         <div className='section-links'>
-          <a href='#welcome-section' className='nav-link'>About</a>
-          <a href='#projects-section' className='nav-link'>Projects</a>
-          <a href='#testimonials-section' className='nav-link'>Testimonials</a>
-          <a href='#contact-section' className='nav-link'>Contact</a>
+          <UnderlineContent>
+            <a href='#welcome-section' className='nav-link'>About</a>
+            <Underline className='underline' />
+          </UnderlineContent>
+          <UnderlineContent>
+            <a href='#projects-section' className='nav-link'>Projects</a>
+            <Underline className='underline' />
+          </UnderlineContent>
+          <UnderlineContent>
+            <a href='#testimonials-section' className='nav-link'>Testimonials</a>
+            <Underline className='underline' />
+          </UnderlineContent>
+          <UnderlineContent>
+            <a href='#contact-section' className='nav-link'>Contact</a>
+            <Underline className='underline' />
+          </UnderlineContent>
         </div>
       </div>
     </Nav>
@@ -38,7 +50,7 @@ const Nav = styled.nav`
   top: 0px;
   background-color: var(--redBG);
   box-shadow: 0 2px 5px var(--blackBG);
-  padding: 0 40px;
+  padding: 0 90px;
 
   h1, h2 {
     cursor: pointer;
@@ -53,30 +65,23 @@ const Nav = styled.nav`
     font-size: 18px;
   }
 
-  .nav-link:not(:last-child) {
-    padding-right: 40px;
-  }
-
   svg {
     display: none;
+    color: white;
+    font-size: 1.4em;
     cursor: pointer;
   }
 
+  div:not(:last-child) {
+    margin-right: 30px;
+  }
+
   @media (max-width: 800px) {
-    .section-links {
-      display: none;
-    }
-
-    svg {
-      display: block;
-    }
-
-    .nav-menu:hover > .section-links {
-      display: flex;
-    }
+    padding: 0 30px;
 
     .section-links {
       position: absolute;
+      display: none;
       flex-direction: column;
       align-items: center;
       background-color: var(--redBG);
@@ -86,11 +91,46 @@ const Nav = styled.nav`
       width: 100%;
       padding: 20px 0;
 
-      .nav-link {
-        padding: 0;
+      div {
+        margin: 0;
       }
     }
+
+    svg {
+      display: block;
+    }
+
+    .nav-menu:hover > .section-links {
+      display: flex;
+    }
   }
+`;
+
+const UnderlineContent = styled.div`
+  position: relative;
+  display: inline-block;
+
+  .nav-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: var(--navbar-height);
+    font-size: 1.2em;
+  }
+
+  &:hover .underline {
+    width: 100%;
+  }
+`;
+
+const Underline = styled.div`
+  position: absolute;
+  width: 0%;
+  height: 6px;
+  bottom: 0;
+  background-color: white;
+  transition: width 0.3s;
+  border-radius: 2px;
 `;
 
 export default NavBar;
